b7397d143e3ef8dd12e59aed799b501e3bbab66e
renardeinside <polarpersonal@gmail.com>
2024-04-05 10:57:20 +0200

    misc fixes for writer

--- src/streaming_pkg_with_dab/tasks/writer.py
+++ src/streaming_pkg_with_dab/tasks/writer.py
@@ -13,7 +13,6 @@ class Writer(Task):
         bootstrap_server = self.get_bootstrap_server()
         logger.info(f"Starting kafka producer to {bootstrap_server}")
         producer = KafkaProducer(bootstrap_servers=self.get_bootstrap_server())
-        producer.send("foobar", b"some_message_bytes")
 
     def get_bootstrap_server(self):
         return "localhost:9092"
